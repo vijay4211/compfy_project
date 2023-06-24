ef62ce9f5532632608ab447ccca95f4a0011058b
vijay4211 <vijayppandit2042@gmail.com>
2023-06-24 16:16:00 +0530

    Hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,6 @@ import styled from 'styled-components'
 import { Link } from 'react-router-dom'
 import heroBcg from '../assets/hero-bcg.jpeg'
 import heroBcg2 from '../assets/hero-bcg-2.jpeg'
-
 const Hero = () => {
   return (
     <Wrapper className="section-center">
@@ -13,40 +12,91 @@ const Hero = () => {
           comfort zone
         </h1>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sed
-          maximus mi. Sed mattis, lectus in tempor rhoncus, nulla metus
-          malesuada lacus, vel dapibus elit dui quis ligula. Proin
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, at
+          sed omnis corporis doloremque possimus velit! Repudiandae nisi odit,
+          aperiam odio ducimus, obcaecati libero et quia tempora excepturi quis
+          alias?
         </p>
-        <Link to="./products" className="btn hero-btn">
+        <Link to="/products" className="btn hero-btn">
           shop now
         </Link>
       </article>
       <article className="img-container">
         <img src={heroBcg} alt="nice table" className="main-img" />
-        <img src={heroBcg2} alt="person working here" className="accent-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
       </article>
     </Wrapper>
   )
 }
 
 const Wrapper = styled.section`
-min-height:60vh;
-display:grid;
-border:1px solid blue;
-place-items:center;
-.img-container{
-  display:none;
-}
-p{
-  line-height:2;
-  max-width:45rem;
-  margin-bottom:2rem;
-  color:var(--clr-grey-5;
-  font-size:1rem;
-}
+  min-height: 60vh;
+  border: 3px solid blue;
+  display: grid;
+  place-items: center;
+  .img-container {
+    display: none;
+  }
 
-
-
+  p {
+    line-height: 2;
+    max-width: 45em;
+    margin-bottom: 2rem;
+    color: var(--clr-grey-5);
+    font-size: 1rem;
+  }
+  @media (min-width: 992px) {
+    //Wrapper
+    height: calc(100vh - 5rem);
+    display: grid;
+    place-items: center;
+    grid-template-columns: 1fr 1fr;
+    gap: 8rem;
+    h1 {
+      margin-bottom: 2rem;
+    }
+    p {
+      font-size: 1.25rem;
+    }
+    .hero-btn {
+      padding: 0.75rem 1.5rem;
+      font-size: 1rem;
+    }
+    .img-container {
+      border: 5px dotted blue;
+      display: block;
+      position: relative;
+    }
+    .main-img {
+      border: 3px solid brown;
+      width: 100%;
+      height: 550px;
+      position: relative;
+      border-radius: var(--radius);
+      display: block;
+      object-fit: cover;
+    }
+    .accent-img {
+      border: 3px solid red;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 250px;
+      transform: translateX(-50%);
+      border-radius: var(--radius);
+    }
+    .img-container::before {
+      border: 3px solid green;
+      content: '';
+      position: absolute;
+      width: 10%;
+      height: 80%;
+      background: var(--clr-primary-9);
+      bottom: 0%;
+      left: -8%;
+      border-radius: var(--radius);
+    }
+  }
 `
 
 export default Hero
